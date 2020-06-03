aff25270aec16431535ff3fb024c432e9e6d0d70
Braian Lopez <brianlopez100894@gmail.com>
2020-06-03 07:27:47 -0300

    🖌 minimal changes

--- index.js
+++ index.js
@@ -3,8 +3,5 @@ const app = require("./src/app");
 app.listen(process.env.PORT_SERVER || 4000, (err) => {
   if (err) console.log("Error");
 
-  console.log(
-    "Server corriendo en el puerto ",
-    process.env.PORT_SERVER || 4000
-  );
+  console.log("server on port ", process.env.PORT_SERVER || 4000);
 });
